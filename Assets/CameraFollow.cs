c7b7c45e3ea394c16e7341ef7eda9762b97a1434
alexanderkarpetc <alexander.karpetc@gmail.com>
2020-10-06 22:19:22 +0300

    Rework tiles

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform _player;
+    public float Speed;
+
+    void Start()
+    {
+        _player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        if(_player != null)
+        {
+            var targetPos = new Vector3(_player.position.x, _player.position.y, -10);
+            transform.position = Vector3.Lerp(transform.position, targetPos, Speed);
+        }
+    }
+}
