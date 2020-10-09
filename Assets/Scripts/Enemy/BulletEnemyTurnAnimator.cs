12904c1f504d9f26aa84a41e53ac624b5f835e41
alexanderkarpetc <alexander.karpetc@gmail.com>
2020-10-10 00:41:50 +0300

    Bullet animation

--- Assets/Scripts/Enemy/BulletEnemyTurnAnimator.cs
+++ Assets/Scripts/Enemy/BulletEnemyTurnAnimator.cs
@@ -8,16 +8,11 @@ namespace Enemy
   public class BulletEnemyTurnAnimator : MonoBehaviour
   {
     [SerializeField] private Animator _animator;
-    [SerializeField] private AIDestinationSetter _destinationSetter;
     [SerializeField] private AIPath _aiPath;
     private int _currentTurn;
     private bool _previousRunState;
     private bool _isRunning;
 
-    private void Start()
-    {
-      _destinationSetter.target = GameObject.Find("Player").transform;
-    }
 
     void Update()
     {
@@ -30,35 +25,30 @@ namespace Enemy
       _isRunning = true;
       Vector2 direction = destination - transform.position;
       var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-      Debug.Log(angle);
-      if (angle >= -75 && angle < 25)
+      if (angle >= -90 && angle < 10)
       {
-        ProcessTurn(EnemyAnimState.downRight, 1);
+        ProcessTurn(EnemyAnimState.downRight);
         return;
       }
 
-      if (angle >= 25 && angle < 70)
+      if (angle >= 10 && angle < 90)
       {
-        ProcessTurn(EnemyAnimState.upRight, 1);
+        ProcessTurn(EnemyAnimState.upRight);
         return;
       }
 
-      if (angle >= 70 && angle < 110)
+      if (angle >= 90 && angle < 170)
       {
-        ProcessTurn(EnemyAnimState.downLeft, 1);
+        ProcessTurn(EnemyAnimState.upLeft);
         return;
       }
 
-      if (angle >= 110 && angle < 155)
-      {
-        ProcessTurn(EnemyAnimState.upLeft, -1);
-        return;
-      }
+      ProcessTurn(EnemyAnimState.downLeft);
     }
 
-    private void ProcessTurn(int direction, int scale)
+    private void ProcessTurn(int direction)
     {
-      if (_currentTurn == direction && _previousRunState == _isRunning)
+      if (_currentTurn == direction)
         return;
       _currentTurn = direction;
       TurnTo(_isRunning);
@@ -71,6 +61,7 @@ namespace Enemy
         _animator.SetTrigger(_currentTurn);
         return;
       }
+
       _animator.SetTrigger(_currentTurn);
     }
   }
---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using Pathfinding;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField] private AIDestinationSetter _destinationSetter;
+        [SerializeField] private AIPath _aiPath;
+        [SerializeField] private float speed;
+
+        private void Start()
+        {
+            _destinationSetter.target = GameObject.Find("Player").transform;
+            _aiPath.maxSpeed = speed;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
