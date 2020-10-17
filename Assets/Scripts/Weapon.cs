64c303b43d17d55eb0405b2d9ff2d6a4f992cf8c
alexanderkarpetc <alexander.karpetc@gmail.com>
2020-10-17 13:08:04 +0300

    Add shoot animation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
@@ -9,26 +11,46 @@ public class Weapon : MonoBehaviour
   [SerializeField] private Transform _shootPoint;
   [SerializeField] private float _damage;
   [SerializeField] private float _shootRate;
-  [SerializeField] private float _reloadTime;
-
-
+  [SerializeField] private int _magazineSize;
+  [SerializeField] private Animator _animator;
+  
+  private int bulletsLeft;
   protected float _nextShotTime;
   protected bool _reloading;
+  private static readonly int Reload = Animator.StringToHash("Reload");
+  private static readonly int Shoot = Animator.StringToHash("Shoot");
 
-  // Update is called once per frame
-  protected virtual void Update()
+  private void Start()
   {
+    bulletsLeft = _magazineSize;
   }
 
   public void TryShoot()
   {
+    if (bulletsLeft <= 0)
+    {
+      StartCoroutine(DoReload());
+
+      return;
+    }
     if (!(Time.time >= _nextShotTime) || _reloading) 
       return;
       
+    _animator.SetTrigger(Shoot);
     var go = Instantiate(_projectile, _shootPoint.position, transform.rotation);
     var projectile = go.GetComponent<Projectile>();
     projectile.IsInverted = IsInverted;
     projectile.IsPlayerBullet = IsPlayers;
     _nextShotTime = Time.time + _shootRate;
+    bulletsLeft--;
+  }
+
+  private IEnumerator DoReload()
+  {
+    _reloading = true;
+    _animator.SetTrigger(Reload);
+    yield return new WaitForSeconds(_animator.GetCurrentAnimatorClipInfo(0).Length);
+    bulletsLeft = _magazineSize;
+    _reloading = false;
   }
 }
