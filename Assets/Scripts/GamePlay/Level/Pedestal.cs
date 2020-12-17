e8783b934a778085989765ddf065ef3b705c85d9
alexanderkarpetc <alexander.karpetc@gmail.com>
2020-12-17 19:43:47 +0200

    Add new room and some seeker adjustments

--- Assets/Scripts/GamePlay/Level/Pedestal.cs
+++ Assets/Scripts/GamePlay/Level/Pedestal.cs
@@ -16,9 +16,9 @@ namespace GamePlay.Level
       _itemSprite.sprite = Item._uiImage;
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-      if (other.collider.CompareTag("Player"))
+      if (other.CompareTag("Player"))
       {
         var newWeapon = Item.GetComponent<Weapon>();
         if (newWeapon)
