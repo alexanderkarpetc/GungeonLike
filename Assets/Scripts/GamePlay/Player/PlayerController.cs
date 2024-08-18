a41530dcf920058a78dcb52830aceb94ffe91a22
Oleksandr Karpets <o.karpets@shgames.gg>
2024-08-18 19:45:34 +0200

    Add inertia

--- Assets/Scripts/GamePlay/Player/PlayerController.cs
+++ Assets/Scripts/GamePlay/Player/PlayerController.cs
@@ -1,83 +1,97 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using GamePlay.Common;
 using GamePlay.Weapons;
 using UnityEngine;
 
 namespace GamePlay.Player
 {
-  public class PlayerController : MonoBehaviour
-  {
-    [HideInInspector] public bool IsBusy;
-
-    [SerializeField] private PlayerAnimatorView playerAnimatorView;
-    [SerializeField] private Rigidbody2D _rigidbody;
-    [SerializeField] private SpriteRenderer _body;
-    [SerializeField] private Weapon _startingWeapon;
-    private int _verticalMove;
-    private int _horizontalMove;
-    private bool _isInvincible;
-    private PlayerInitializer _initializer = new PlayerInitializer();
-    
-    public void Init()
+    public class PlayerController : MonoBehaviour
     {
-      _initializer.Init(_startingWeapon);
-    }
+        [HideInInspector] public bool IsBusy;
 
-    private void Update()
-    {
-      ReadInput();
-    }
+        [SerializeField] private PlayerAnimatorView playerAnimatorView;
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private SpriteRenderer _body;
+        [SerializeField] private Weapon _startingWeapon;
+        
+        private int _verticalMove;
+        private int _horizontalMove;
+        private bool _isInvincible;
+        private PlayerInitializer _initializer = new PlayerInitializer();
+        
+        private readonly float Inertia = 0.2f;
 
-    private void ReadInput()
-    {
-      var a = Input.GetKey(KeyCode.A) ? 1 : 0;
-      var d = Input.GetKey(KeyCode.D) ? 1 : 0;
-      _horizontalMove = d - a;
-      var w = Input.GetKey(KeyCode.W) ? 1 : 0;
-      var s = Input.GetKey(KeyCode.S) ? 1 : 0;
-      _verticalMove = w - s;
-      if (IsBusy)
-      {
-        _horizontalMove = 0;
-        _verticalMove = 0;
-      }
-      playerAnimatorView.HorizontalMove = _horizontalMove;
-      playerAnimatorView.VerticalMove = _verticalMove;
-      Move();
-    }
+        public void Init()
+        {
+            _initializer.Init(_startingWeapon);
+        }
 
-    private void Move()
-    {
-      _rigidbody.velocity = new Vector2(_horizontalMove, _verticalMove).normalized * StaticData.PlayerSpeedBase * AppModel.Player().SpeedMultiplier;
-    }
+        private void Update()
+        {
+            ReadInput();
+        }
 
-    public void Hit()
-    {
-      if(!_isInvincible)
-        StartCoroutine(ApplyHit());
-    }
+        private void ReadInput()
+        {
+            if (IsBusy)
+            {
+                _horizontalMove = 0;
+                _verticalMove = 0;
+            }
+            else
+            {
+                _horizontalMove = Input.GetKey(KeyCode.D) ? 1 : (Input.GetKey(KeyCode.A) ? -1 : 0);
+                _verticalMove = Input.GetKey(KeyCode.W) ? 1 : (Input.GetKey(KeyCode.S) ? -1 : 0);
+            }
 
-    private IEnumerator ApplyHit()
-    {
-      _isInvincible = true;
-      var state = AppModel.Player();
-      state.DealDamage();
-      if (state.GetHp() <= 0)
-        Die();
-      // ScreenBlink
-      var bodyColor = _body.color;
-      bodyColor.a = 0.5f;
-      _body.color = bodyColor;
-      yield return new WaitForSeconds(1f);
-      bodyColor.a = 1;
-      _body.color = bodyColor;
-      _isInvincible = false;
-    }
+            playerAnimatorView.HorizontalMove = _horizontalMove;
+            playerAnimatorView.VerticalMove = _verticalMove;
+        }
 
-    private void Die()
-    {
-      Destroy(gameObject);
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            Vector2 targetVelocity = new Vector2(_horizontalMove, _verticalMove).normalized * StaticData.PlayerSpeedBase * AppModel.Player().SpeedMultiplier;
+            _rigidbody.velocity = Vector2.Lerp(_rigidbody.velocity, targetVelocity, Inertia);
+        }
+
+        public void Hit()
+        {
+            if (!_isInvincible)
+                StartCoroutine(ApplyHit());
+        }
+
+        private IEnumerator ApplyHit()
+        {
+            _isInvincible = true;
+            var state = AppModel.Player();
+            state.DealDamage();
+
+            if (state.GetHp() <= 0)
+            {
+                Die();
+                yield break;
+            }
+
+            // ScreenBlink
+            var bodyColor = _body.color;
+            bodyColor.a = 0.5f;
+            _body.color = bodyColor;
+
+            yield return new WaitForSeconds(1f);
+
+            bodyColor.a = 1f;
+            _body.color = bodyColor;
+            _isInvincible = false;
+        }
+
+        private void Die()
+        {
+            Destroy(gameObject);
+        }
     }
-  }
 }
